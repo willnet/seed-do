4e6a0eb1000ae8ef6c5c181dfd55beeadc506c17
Shinichi Maeshima <netwillnet@gmail.com>
2025-05-22 06:54:31 +0900

    Fix a deprecation warning about using `should`
    Fix a deprecation warning about using should that appeared when running tests.
    
    ```
    Deprecation Warnings:
    
    Using `should` from rspec-expectations' old `:should` syntax without explicitly enabling the syntax is deprecated. Use the new `:expect` syntax or explicitly enable `:should` with `config.expect_with(:rspec) { |c| c.syntax = :should }` instead. Called from /home/runner/work/seed-do/seed-do/spec/runner_spec.rb:7:in `block (2 levels) in <top (required)>'.
    
    If you need more of the backtrace for any of these deprecations to
    identify where to make the necessary changes, you can configure
    `config.raise_errors_for_deprecations!`, and it will turn the
    deprecation warnings into errors, giving you the full backtrace.
    ```
    
    Use synvert to change `should` to `expect`
    
    `synvert-ruby -r rspec/should_to_expect .`

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -4,21 +4,21 @@ describe SeedDo::Runner do
   it "should seed data from Ruby and gzipped Ruby files in the given fixtures directory" do
     SeedDo.seed(File.dirname(__FILE__) + '/fixtures')
 
-    SeededModel.find(1).title.should == "Foo"
-    SeededModel.find(2).title.should == "Bar"
-    SeededModel.find(3).title.should == "Baz"
+    expect(SeededModel.find(1).title).to eq "Foo"
+    expect(SeededModel.find(2).title).to eq "Bar"
+    expect(SeededModel.find(3).title).to eq "Baz"
   end
 
   it "should seed only the data which matches the filter, if one is given" do
     SeedDo.seed(File.dirname(__FILE__) + '/fixtures', /_2/)
 
-    SeededModel.count.should == 1
-    SeededModel.find(2).title.should == "Bar"
+    expect(SeededModel.count).to eq 1
+    expect(SeededModel.find(2).title).to eq "Bar"
   end
 
   it "should use the SeedDo.fixtures_paths variable to determine where fixtures are" do
     SeedDo.fixture_paths = [File.dirname(__FILE__) + '/fixtures']
     SeedDo.seed
-    SeededModel.count.should == 3
+    expect(SeededModel.count).to eq 3
   end
 end
